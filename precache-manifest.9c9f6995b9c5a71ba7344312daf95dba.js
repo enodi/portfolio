6377c0b089c60e0bb66c75bc50033714a0e90c71
enodi <enodiaudu5@gmail.com>
2020-08-24 00:58:13 +0100

    Updates

---
+++ precache-manifest.9c9f6995b9c5a71ba7344312daf95dba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cae19e2e8b55f2465b6f7a755f573d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f6a859e94b80835822a3",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5ea777af06ba93706c04",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6a859e94b80835822a3",
+    "url": "/portfolio/static/js/2.d3cec4ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d3cec4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea777af06ba93706c04",
+    "url": "/portfolio/static/js/main.db8995d9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "90c08184b6b9742f1fbfcd0aa3b8ff32",
+    "url": "/portfolio/static/media/1.90c08184.png"
+  },
+  {
+    "revision": "9520ea51bf3c3d9836aba819e75710ea",
+    "url": "/portfolio/static/media/2.9520ea51.png"
+  },
+  {
+    "revision": "98935f12f49f5911b4317e1ae68b0242",
+    "url": "/portfolio/static/media/3.98935f12.png"
+  },
+  {
+    "revision": "3be5180da5ddf49574e25c2e1da39946",
+    "url": "/portfolio/static/media/4.3be5180d.png"
+  },
+  {
+    "revision": "de3c0fb2cfe6640552cbc5bd0a3e1742",
+    "url": "/portfolio/static/media/5.de3c0fb2.png"
+  },
+  {
+    "revision": "e34ccb02d72acd373361a9dba171ce2f",
+    "url": "/portfolio/static/media/6.e34ccb02.png"
+  },
+  {
+    "revision": "e553f58b3768aff8393f61c427c3e057",
+    "url": "/portfolio/static/media/7.e553f58b.png"
+  },
+  {
+    "revision": "26f5be75ccb9217e14e7ddacb3517205",
+    "url": "/portfolio/static/media/8.26f5be75.png"
+  }
+]);
